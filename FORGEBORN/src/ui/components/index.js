ef3b2f5920c421a49975b2179e5fe33cf587fae8
VedantPanchal23 <vedantpanchal2309@gmail.com>
2026-03-08 23:43:20 +0530

    refactor(ui): export new glassmorphic and kinetic components

--- FORGEBORN/src/ui/components/index.js
+++ FORGEBORN/src/ui/components/index.js
@@ -2,6 +2,8 @@ export * from './Card';
 export * from './Button';
 export * from './Typography';
 export * from './ProgressBar';
+export * from './ScreenWrapper';
+export { default as CustomBottomSheet } from './CustomBottomSheet';
 // Exporting existing legacy components carefully
 export { default as CalorieRing } from './CalorieRing';
 export { default as MacroBar } from './MacroBar';
